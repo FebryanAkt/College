cb8b92f44d0ea6673585d6e4d57279c291d486d6
FebryanAkt <febryan08022006@gmail.com>
2025-02-06 08:44:19 +0700

    learn type data to function

---
+++ Semester 1_Dasar Pemrograman/Jobsheet 1/MyFirstJava10.java
@@ -0,0 +1,22 @@
+public class MyFirstJava10 {
+    public static void main(String[] args) {
+        
+        System.out.println("---------------------------------------------------------");
+        System.out.println("-- Data Mahasiswa Jurusan Teknologi Informasi POLINEMA --");
+        System.out.println("---------------------------------------------------------");
+        System.out.println("Nama    : Febryan Akhmad Taajuddin bin subkhan");
+        System.out.println("NIM     : 244107020180");
+        System.out.println("Kelas   : 1E");
+        System.out.println("No Absen: 10");
+        System.out.println("Prodi   : D-IV Teknik Informatika");
+        System.out.println("Alamat  : JL. Untung Suropati Sel 5");
+        System.out.println("No HP   : 085840625208");
+        System.out.println("Hobi    : Membaca novel");
+
+
+
+        System.out.print("Nama saya Febryan Akhmad Taajuddin");
+        System.out.print("Saya adalah mahasiswa baru Jurusan Teknologi Informasi");
+
+    }
+}
